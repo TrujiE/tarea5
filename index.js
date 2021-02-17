3fbd0ba9c889f119107847cc430e62249d4ca145
Aaron Trujillo <atrujilloembry@gmail.com>
2021-02-17 17:40:28 -0300

    Excusa

---
+++ index.js
@@ -0,0 +1,11 @@
+	import '../style/index.scss';
+
+	function onLoad(){
+
+		let random = Math.random()*10;
+		let who = ['the dog','my granma','his turtle','my bird'];
+		let what = ['eat','pissed','crushed','broked'];
+		let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+		console.log('HOLA' + random);
+	}
