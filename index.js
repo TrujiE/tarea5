afc6d56aac9c9689ed5d428903a3ce404a8dd72e
Aaron Trujillo <atrujilloembry@gmail.com>
2021-02-17 21:03:36 -0300

    tarea excusa se agrega botón y cambia el contenido

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-	import '../style/index.scss';
+	
 
 	function onLoad(){
 
-		let random = Math.random()*10;
 		let who = ['the dog','my granma','his turtle','my bird'];
+		let cuenta_who = who.length;
+		let random_who = Math.floor(Math.random() * cuenta_who)  ;
+		
 		let what = ['eat','pissed','crushed','broked'];
+		let cuenta_what = what.length;
+		let random_what = Math.floor(Math.random() * cuenta_what)  ;
+		
 		let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+		let cuenta_when = when.length;
+        let random_when = Math.floor(Math.random() * cuenta_when)  ;
 
-		console.log('HOLA' + random);
-	}
+      	let element = document.getElementById("excusa");
+		element.innerHTML= who[random_who] + " " + what[random_what] + " " + when[random_when];
+     
+      	//return element.innerHTML;
+  
+     }
+
+
+ 		
+		
+
+
